7121acda799f52cfb976fe3af647a99c1ab1d3f4
Sergey Savtyra <drunkypanda0806@yandex.ru>
2016-12-17 22:11:26 +0300

    Update password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,34 @@
+import re
 
 
-def get_password_strength(password):
-    pass
+def load_password_blacklist(filepath):
+    with open(filepath, 'r') as blacklist_data:
+        prohibitted_passwords = blacklist_data.read()
+        prohibitted_passwords = re.findall(r'[\w]+', prohibitted_passwords)
+        return prohibitted_passwords
+
+
+def get_password_strength(password, blacklist):
+    if len(password) < 6:
+        return 1
+    else:
+        password_strength = 1
+        if password.lower() != password and password.upper() != password: 
+            password_strength += 1
+        if re.findall(r'[\d]', password): 
+            password_strength += 1
+        if re.findall(r'[!@#$%^&*><}{]', password):
+            password_strength += 2
+        if password not in blacklist:
+            password_strength += 3
+        if len(password) > 12:  
+            password_strength += 2
+        return (password_strength)
 
 
 if __name__ == '__main__':
-    pass
+    blacklist_path = input("Enter filepath to the blacklist: \n")
+    password = input("Enter you password: \n")
+    blacklist = load_password_blacklist(blacklist_path)
+    rate = get_password_strength(password, blacklist)
+    print("Your password is {}{}".format(str(rate), "/10"))
